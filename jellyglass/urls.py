3162576765da3de7573711b33b256b5b9039675b
Markus Holtermann <info@markusholtermann.eu>
2016-01-14 20:29:46 +1100

    Added Wordpress jellyglass

--- jellyglass/urls.py
+++ jellyglass/urls.py
@@ -13,6 +13,16 @@ def get_urls():
             url(r'^admin/login/$', views.django_admin_login, name='django_admin_login'),
         ])
 
+    if getattr(settings, 'JELLYGLASS_WORDPRESS', True):
+        patterns.extend([
+            url(r'^wp-admin/$', views.wordpress_admin, name='wordpress_admin'),
+            url(r'^wp-admin.php$', views.wordpress_admin_login, name='wordpress_admin_login'),
+            url(r'^wp-includes/css/buttons.min.css$', views.static, {'path': 'css/wordpress/buttons.min.css'}),
+            url(r'^wp-includes/css/dashicons.min.css$', views.static, {'path': 'css/wordpress/dashicons.min.css'}),
+            url(r'^wp-admin/css/login.min.css$', views.static, {'path': 'css/wordpress/login.min.css'}),
+            url(r'^wp-admin/images/wordpress-logo.svg$', views.static, {'path': 'img/wordpress/wordpress-logo.svg'}),
+        ])
+
     return patterns
 
 urlpatterns = get_urls()
--- jellyglass/views.py
+++ jellyglass/views.py
@@ -1,5 +1,10 @@
+import functools
+
+from django.contrib.staticfiles.views import serve
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect, render
+from django.utils.http import urlquote
+from django.views.decorators.csrf import csrf_exempt
 
 from .utils import record_spoon
 
@@ -12,3 +17,24 @@ def django_admin(request):
 @record_spoon('django')
 def django_admin_login(request):
     return render(request, 'jellyglass/django.html')
+
+
+def wordpress_admin(request):
+    path = reverse('jellyglass:wordpress_admin_login')
+    to = '{scheme}://{host}{path}?redirect_to={current_uri}&reauth=1'.format(
+        scheme=request.scheme, host=request.get_host(),
+        path=path, current_uri=urlquote(request.build_absolute_uri(), safe=None),
+    )
+    return redirect(to)
+
+
+@csrf_exempt
+@record_spoon('wordpress')
+def wordpress_admin_login(request):
+    context = {
+        'domain': '{scheme}://{host}'.format(scheme=request.scheme, host=request.get_host()),
+    }
+    return render(request, 'jellyglass/wordpress.html', context)
+
+
+static = functools.partial(serve, insecure=True)
