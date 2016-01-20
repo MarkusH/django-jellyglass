a1ea0bc80911836bedece8d3168d1c2f92831369
Markus Holtermann <info@markusholtermann.eu>
2016-01-20 11:23:49 +1100

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*parts):
 
 setup(
     name='django-jellyglass',
-    version='0.2.1',
+    version='0.2.2',
     description='The ultimate Django admin honeypot.',
     long_description=read('README.rst'),
     author='Markus Holtermann',
